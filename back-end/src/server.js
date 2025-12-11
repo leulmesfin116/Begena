cfe3c73bf7e215a82eb7bcb5362368872b9b93e4
leulmesfin116 <leul116hga@gmail.com>
2025-12-11 22:21:27 +0300

    update the backk end

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 5007;
+
+//Get the file path from the url of the current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name from the file path
+const __dirname = dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "front-end", "begena-prj", "src", "pages", "Home.jsx")
+  );
+});
 
 app.listen(PORT, () => {
   console.log(`The serevr has started at Port:${PORT}`);
