7cd96db37b7f38d4c2277ec8b34adc9499c3a75b
leulmesfin116 <leul116hga@gmail.com>
2025-12-23 10:33:40 +0300

    updating the gitgore

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import pool from "./db.js";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./Routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5007;
@@ -12,6 +13,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // middlwWare
 app.use(express.json());
+// Routes
+app.use("/auth", authRoutes);
 //to show the file path directory
 app.use(
   express.static(path.join(__dirname, "../../front-end/begena-prj/dist"))
