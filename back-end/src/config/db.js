c540314813d17250241c8dba6e3e2d642b15725a
leulmesfin116 <leul116hga@gmail.com>
2026-01-30 12:43:46 +0300

    connecting the databse with the prisma

--- back-end/src/config/db.js
+++ back-end/src/config/db.js
@@ -6,3 +6,18 @@ const prisma = new prismaClient({
       ? ["query", "error", "warn"]
       : ["error"],
 });
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log(`DB connected via prisma`);
+  } catch (error) {
+    console.log(`Data base connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
+export { prisma, connectDB, disconnectDB };
