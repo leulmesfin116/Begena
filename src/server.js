24ce70f1d61ce31f1c8809541b6de3943b013802
leulmesfin116 <leul116hga@gmail.com>
2026-02-11 10:09:08 +0300

    updating the errors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+import "dotenv/config";
 import express from "express";
+import { connectDB, disconnectDB } from "./config/db.js";
 
 import songRoute from "./routes/songRoutes.js";
+
+connectDB();
 
 const app = express();
 
@@ -10,4 +14,28 @@ const PORT = 5000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
+});
+
+// handle unhandke promise rejection
+process.on("unhandledRejection", (err) => {
+  console.error("unhandledRejection", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+// handle uncaught exception
+process.on("uncaughtException", async (err) => {
+  console.error("uncaughtException", err);
+  await disconnectDB();
+  process.exit(1);
+});
+// graceful shutdown
+process.on("SIGTERM", async () => {
+  console.log("SIGTERM recieved,shutting down gracefully");
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  });
 });
