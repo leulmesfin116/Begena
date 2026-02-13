42124492dad4152bd508aa691986f6d0bb8961ba
leulmesfin116 <leul116hga@gmail.com>
2026-02-13 21:24:42 +0300

    hashing the password

--- back-end/src/controller/authController.js
+++ back-end/src/controller/authController.js
@@ -1,4 +1,5 @@
 import { prisma } from "../config/db.js";
+import bcrypt from "bcryptjs";
 const register = async (req, res) => {
   const { name, password, email } = req.body;
 
@@ -6,8 +7,12 @@ const register = async (req, res) => {
     where: { email: email },
   });
   if (userExist) {
-    return res.status(400).json({ error: "user alrady exist with this password" });
+    return res
+      .status(400)
+      .json({ error: "user alrady exist with this password" });
   }
 };
-
+// hash password
+const salt = await bcrypt.genSalt(11);
+const hashedPassword = await bcrypt.hash(password, salt);
 export { register };
