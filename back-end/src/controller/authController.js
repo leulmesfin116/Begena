ca40fefb8f1a8d62a40e5398afa02cf2a4cd2e23
leulmesfin116 <leul116hga@gmail.com>
2026-02-14 09:44:18 +0300

    login authethication

--- back-end/src/controller/authController.js
+++ back-end/src/controller/authController.js
@@ -11,24 +11,34 @@ const register = async (req, res) => {
       .status(400)
       .json({ error: "user alrady exist with this password" });
   }
+  // hash password
+  const salt = await bcrypt.genSalt(11);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  // creating a user
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+  res.status(201).json({
+    satus: "success",
+    data: {
+      id: user.id,
+      name: name,
+      email: email,
+    },
+  });
 };
-// hash password
-const salt = await bcrypt.genSalt(11);
-const hashedPassword = await bcrypt.hash(password, salt);
-// creating a user
-const user = await prisma.user.create({
-  data: {
-    name,
-    email,
-    password: hashedPassword,
-  },
-});
-res.status(201).json({
-  satus: "success",
-  data: {
-    id: user.id,
-    name: name,
-    email: email,
-  },
-});
+// Login functionality
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: { email: email },
+  });
+  if (!user) {
+    return res.status(400).json({ message: "invalid email and password" });
+  }
+};
 export { register };
