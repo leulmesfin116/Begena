5ff6587a09a3225bf2d946f275722da30d148e67
leulmesfin116 <leul116hga@gmail.com>
2026-02-15 09:12:16 +0300

    solving the token error

--- back-end/src/utils/generateToken.js
+++ back-end/src/utils/generateToken.js
@@ -5,14 +5,11 @@ export const generateToken = (userId, res) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-  res.cookie(
-    "jwt",
-    token({
-      httpOnly: true,
-      secure: (process.env.NODE_ENV = "production"),
-      samesite: "strict",
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    }),
-  );
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: (process.env.NODE_ENV = "production"),
+    samesite: "strict",
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  });
   return token;
 };
