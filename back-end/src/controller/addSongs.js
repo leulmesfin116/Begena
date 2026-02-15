cb154a9ae20f99379fb14dddaa8d72d7a3600222
leulmesfin116 <leul116hga@gmail.com>
2026-02-15 10:31:55 +0300

    adding the songs

--- back-end/src/controller/addSongs.js
+++ back-end/src/controller/addSongs.js
@@ -1,3 +1,20 @@
-import { Prisma } from "../config/db.js";
+import { prisma } from "../config/db.js";
 // adding song
-const addSong = async (req, res) => {};
+const song = async (req, res) => {
+  try {
+    const { title, artist, posterUrl } = req.body;
+    // validationg the input
+    if (!title || !artist || !posterUrl) {
+      return res.status(400).json({ message: "All the fields are required" });
+    }
+    // adding new song
+    const newSong = await prisma.song.create({
+      data: { title, artist, posterUrl },
+    });
+    res.status(201).json({ song: newSong });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "server error" });
+  }
+};
+export { song };
