edd178f5a1db9681d1beb86dfb62c4d7c265bb43
leulmesfin116 <leul116hga@gmail.com>
2026-02-15 18:56:59 +0300

    updating tthe favourite song

--- back-end/src/controller/favSongController.js
+++ back-end/src/controller/favSongController.js
@@ -1,2 +1,21 @@
-const addtoWatchList = async (req, res) => {};
+import { prisma } from "../config/db.js";
+const addtoWatchList = async (req, res) => {
+  const { userId, songId } = req.body;
+  // adding a song
+  const songExist = await prisma.favouriteSong.findUnique({
+    where: {
+      userId_songId: {
+        userId,
+        songId,
+      },
+    },
+  });
+  if (songExist) {
+    return res.status(400).json({ message: "The song already exits" });
+  }
+  const addSong = await prisma.favouriteSong.create({
+    data: { userId, songId },
+  });
+  res.status(201).json({ message: "song added to the favourite" });
+};
 export { addtoWatchList };
