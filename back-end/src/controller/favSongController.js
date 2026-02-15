b627bd9a24129be912d697a40f93d085916c74cc
leulmesfin116 <leul116hga@gmail.com>
2026-02-15 19:26:06 +0300

    updating

--- back-end/src/controller/favSongController.js
+++ back-end/src/controller/favSongController.js
@@ -1,21 +1,32 @@
 import { prisma } from "../config/db.js";
 const addtoWatchList = async (req, res) => {
   const { userId, songId } = req.body;
-  // adding a song
-  const songExist = await prisma.favouriteSong.findUnique({
-    where: {
-      userId_songId: {
-        userId,
-        songId,
+  try {
+    // check if the song exist on the table
+    const song = await prisma.song.findUnique({ where: { id: songId } });
+    if (!song) {
+      return res.status(404).json({ message: "Song not found" });
+    }
+    // adding a song
+    const songExist = await prisma.favouriteSong.findUnique({
+      where: {
+        userId_songId: {
+          userId,
+          songId,
+        },
       },
-    },
-  });
-  if (songExist) {
-    return res.status(400).json({ message: "The song already exits" });
+    });
+    if (songExist) {
+      return res.status(400).json({ message: "The song already exits" });
+    }
+    const addSong = await prisma.favouriteSong.create({
+      data: { userId, songId },
+    });
+    res.status(201).json({ message: "song added to the favourite" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Something went wrong", error: err.message });
   }
-  const addSong = await prisma.favouriteSong.create({
-    data: { userId, songId },
-  });
-  res.status(201).json({ message: "song added to the favourite" });
 };
 export { addtoWatchList };
