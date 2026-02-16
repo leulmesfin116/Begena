0c775ab7b048bf5c2fe1b71c083af7282515be2b
leulmesfin116 <leul116hga@gmail.com>
2026-02-16 19:49:11 +0300

    updating

--- back-end/src/routes/recentRoute.js
+++ back-end/src/routes/recentRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
 
 const router = express.Router();
+import { recent } from "../controller/recentController.js";
+
+router.get("/played", recent);
 
 export default router;
