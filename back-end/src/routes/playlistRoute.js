3cc84a24045add795f3c487892dffb3d2918caf8
leulmesfin116 <leul116hga@gmail.com>
2026-02-21 19:32:54 +0300

    updating the back end

--- back-end/src/routes/playlistRoute.js
+++ back-end/src/routes/playlistRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authMiddleware from "../middleware/authMiddleware.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 const route = express.Router();
 
 import { playList } from "../controller/playlistController.js";
--- front-end/begena-prj/src/pages/Home.jsx
+++ front-end/begena-prj/src/pages/Home.jsx
@@ -139,7 +139,7 @@ export function Home() {
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6, delay: 1.2 }}
+          transition={{ duration: 0.6, delay: 1.4 }}
           onClick={handleFav}
           className="card animate cursor-pointer sm:col-span-2 lg:col-span-1"
         >
