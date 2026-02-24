6db4b45f3a5179be14f3ad201699c2de4fa795ce
leulmesfin116 <leul116hga@gmail.com>
2026-02-24 10:39:49 +0300

    the recently controller

--- back-end/src/controller/recentController.js
+++ back-end/src/controller/recentController.js
@@ -1,14 +1,22 @@
 import { prisma } from "../config/db.js";
 import jwt from "jsonwebtoken";
-const recent = async(req, res) => {
-  try{
-     const userId=req.user.id;
-  const {songid}=req.body
-const songExist= await prsima.
-  }
-  catch(error){
-
-  }
-
+const recent = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { songId } = req.body;
+    const songExist = await prisma.song.findUnique({
+      where: { id: songId },
+    });
+    if (!songExist) {
+      return res.status(401).json({ message: "the song doesnt exist" });
+    }
+    // adding in to the playlist
+    const newsong = await prisma.recentlyplayed.create({
+      data: {
+        userId: userId,
+        songId,
+      },
+    });
+  } catch (error) {}
 };
 export { recent };
