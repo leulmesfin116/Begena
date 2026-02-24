4c7d291b34b0beafd9927ee5667326dca102f12e
leulmesfin116 <leul116hga@gmail.com>
2026-02-24 09:48:51 +0300

    update

--- back-end/src/controller/playlistController.js
+++ back-end/src/controller/playlistController.js
@@ -2,24 +2,40 @@ import { prisma } from "../config/db.js";
 import jwt from "jsonwebtoken";
 
 const playList = async (req, res) => {
-  const userId = req.user.id;
+  try {
+    const userId = req.user.id;
+    const { name, songId } = req.body;
 
-  const { songId } = req.body;
-  const songExist = await prisma.song.findUnique({
-    where: { songId: songId },
-  });
-  if (!songExist) {
-    return res.status(400).json({ message: "Song does not exist" });
+    const songExist = await prisma.song.findUnique({
+      where: { id: songId },
+    });
+    if (!songExist) {
+      return res.status(400).json({ message: "Song does not exist" });
+    }
+    if (!name || name.trim() === "") {
+      return res.status(400).json({ message: "playlist require name" });
+    }
+    const playlist = await prisma.playlist.create({
+      data: {
+        name,
+        userId: userId,
+      },
+    });
+    await prisma.playlistSong.create({
+      data: {
+        playlistId: playlist.id,
+        songId: songId,
+      },
+    });
+    res.status(201).json({
+      message: "the playlist created",
+      playlist,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "something went wrong",
+      error: error.message,
+    });
   }
-  const { name } = req.body;
-  if (!name || name.trim() === "") {
-    return res.status(400).json({ message: "playlist require name" });
-  }
-  const playlist = await prisma.playlist.create({
-    data: {
-      name,
-      userId: req.user.id,
-    },
-  });
 };
 export { playList };
